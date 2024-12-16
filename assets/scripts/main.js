d8b91a1617801b6ad482e88744b990c0331d4f10
viniciusfariac <vfariacerqueira@gmail.com>
2024-12-15 22:21:58 -0300

    add = color change function

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,4 +1,8 @@
+const theme = document.getElementById("toggleTheme")
 function toggleTheme() {
     document.body.classList.toggle('dark-theme')
     document.body.classList.toggle('light-theme')
+
+    theme.classList.toggle("bi-sun")
+    theme.classList.toggle("bi-moon-stars")
 }
